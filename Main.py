b59661f8aa3958d152d45ca18b21cac990b31a56
fredzs <fred_zs@163.com>
2018-01-25 09:54:53 +0800

    加入Flask

--- Main.py
+++ Main.py
@@ -1,14 +1,23 @@
 import logging
 from logging.config import fileConfig
+from flask import Flask
 
 
 fileConfig('logging_config.ini')
 logger = logging.getLogger()
 
 
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+
 if "__main__" == __name__:
     logger.info('-------------------------------------------'
                 '程序开始执行-------------------------------------------')
-    pass
+    app.run(debug=True)
     logger.info('-------------------------------------------'
                 '程序执行结束-------------------------------------------')
