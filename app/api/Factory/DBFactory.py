c039168f62f233ac04daeb5af6c5f31984ed6d57
fredzs <fredzhangsheng@gmail.com>
2018-02-25 21:55:41 +0800

    调整mysql,pool_recycle=50

--- app/api/Factory/DBFactory.py
+++ app/api/Factory/DBFactory.py
@@ -9,9 +9,8 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 class DBFactory(object):
     """数据库工厂"""
     # 初始化数据库连接:
-    engine = create_engine('mysql+mysqlconnector://fred_zs:some_pass@123.207.136.133:3306/ICBC', pool_recycle=3600)
+    engine = create_engine('mysql+mysqlconnector://fred_zs:some_pass@123.207.136.133:3306/ICBC', pool_recycle=50)
     # 创建DBSession类型:
-    #DBSession = sessionmaker(bind=engine)
     DBSession = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
     # 创建session对象:
     db_session = DBSession()
