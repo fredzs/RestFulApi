398eca2ea7b632c80302c15800fd32d66eec5f15
fredzs <fred_zs@163.com>
2018-03-01 16:41:29 +0800

    运行时增加指定端口的参数

--- manage.py
+++ manage.py
@@ -1,6 +1,12 @@
 from app import create_app
 app = create_app('default')
+import sys
 
 
 if __name__ == '__main__':
-    app.run(ssl_context=("ssl/www.fredirox.com.crt", "ssl/www.fredirox.com.key"))
+    print(sys.argv)
+    if len(sys.argv) > 1:
+        port = int(sys.argv[1])
+    else:
+        port = 5000
+    app.run(port=port, ssl_context=("ssl/www.fredirox.com.crt", "ssl/www.fredirox.com.key"))
